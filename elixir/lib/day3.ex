0204e7213dd0649fc4d86bf1c003605ad64db58d
zcrutcher <50390021+zcrutcher@users.noreply.github.com>
2022-01-22 10:04:41 -0600

    Refactored day3 question 1 to use pattern matching

--- elixir/lib/day3.ex
+++ elixir/lib/day3.ex
@@ -6,44 +6,44 @@ defmodule Day3 do
         |> String.split("", trim: true)
     end
 
-    def question1([head | tail], visited, {x, y}) do
-
-        case head do
-                ">" ->
-                case Enum.member?(visited, [x + 1, y]) do
-                    false ->
-                        question1(tail, [[x + 1, y] | visited], { x + 1, y })
-                    true ->
-                        question1(tail, visited, { x + 1, y })
-                end
-            "<" ->
-                case Enum.member?(visited, [x - 1, y]) do
-                    false ->
-                        question1(tail, [[x - 1, y] | visited], { x - 1, y })
-                    true ->
-                        question1(tail, visited, { x - 1, y })
-                end
-            "^" ->
-                case Enum.member?(visited, [x , y + 1]) do
-                    false ->
-                        question1(tail, [[x , y + 1 ] | visited], { x , y + 1 })
-                    true ->
-                        question1(tail, visited, { x , y + 1 })
-                end
-
-            "v" ->
-                case Enum.member?(visited, [x , y - 1]) do
-                    false ->
-                        question1(tail, [[x , y - 1 ] | visited], { x , y - 1 })
-                    true ->
-                        question1(tail, visited, { x , y - 1 })
-
-                end
-        end        
+    def question1([">" | tail], visited, {x, y}) do
+        case Enum.member?(visited, [x + 1, y]) do
+            false ->
+                question1(tail, [[x + 1, y] | visited], { x + 1, y })
+            true ->
+                question1(tail, visited, { x + 1, y })
+        end
     end
 
-    def question1([], visited, {x,y}) do
-        Enum.count(visited)
+    def question1(["<" | tail], visited, {x, y}) do
+        case Enum.member?(visited, [x - 1, y]) do
+            false ->
+                question1(tail, [[x - 1, y] | visited], { x - 1, y })
+            true ->
+                question1(tail, visited, { x - 1, y })
+        end
+    end
+
+    def question1(["^" | tail], visited, {x, y}) do
+        case Enum.member?(visited, [x , y + 1]) do
+            false ->
+                question1(tail, [[x , y + 1] | visited], { x, y + 1 })
+            true ->
+                question1(tail, visited, { x , y + 1 })
+        end
+    end
+
+    def question1(["v" | tail], visited, {x, y}) do
+        case Enum.member?(visited, [x , y - 1]) do
+            false ->
+                question1(tail, [[x , y - 1 ] | visited], { x , y - 1 })
+            true ->
+                question1(tail, visited, { x , y - 1 })
+        end
+    end
+
+    def question1([], visitied, {x,y}) do
+        Enum.count(visitied)
     end
 
     def answer1 do
