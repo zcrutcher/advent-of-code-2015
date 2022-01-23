8f5f7d090de0d04681d972e5525fee72ceba6fdd
zcrutcher <50390021+zcrutcher@users.noreply.github.com>
2022-01-23 17:57:08 -0600

    Made Day 5 Question 1 a pure function. No more global constant being used without being passed into the function.

--- elixir/lib/day5.ex
+++ elixir/lib/day5.ex
@@ -27,20 +27,20 @@ defmodule Day5 do
         end
     end
 
-    def bad_character?(string) do
-        String.contains?(string, @bad)
+    def bad_character?(string, bad_characters) do
+        String.contains?(string, bad_characters)
     end
 
-    def vowel_check([h | t], count)  do
-        case String.contains?(h, @vowels) do
+    def vowel_check([h | t], count, vowels)  do
+        case String.contains?(h, vowels) do
             true ->
-                vowel_check(t, count + 1)
+                vowel_check(t, count + 1, vowels)
             false ->
-                vowel_check(t, count)
+                vowel_check(t, count, vowels)
         end
     end
 
-    def vowel_check([], count) do
+    def vowel_check([], count, vowels) do
         count
     end
 
@@ -52,37 +52,37 @@ defmodule Day5 do
         end 
     end
 
-    def question1([h | t], count, repeated, bad, answer_count) do
+    def question1([h | t], count, repeated, bad, vowels, bad_characters, answer_count) do
         
         split_input =
         String.split(h, "", trim: true)
         
         vowel_count = 
-        vowel_check(split_input, count)
+        vowel_check(split_input, count, vowels)
 
         [head | tail] = split_input
         repeated =
         double_check(tail, head, repeated)
 
         bad =
-        bad_character?(h)
+        bad_character?(h, bad_characters)
 
         good?(vowel_count, repeated, bad)
         |> case do
             true ->
-                question1(t, 0, false, false, answer_count + 1)
+                question1(t, 0, false, false, vowels, bad_characters, answer_count + 1)
             false ->
-                question1(t, 0, false, false, answer_count)
+                question1(t, 0, false, false, vowels, bad_characters, answer_count)
         end 
     end
 
-    def question1([], count, repeated, bad, answer_count) do
+    def question1([], count, repeated, bad, vowels, bad_characters, answer_count) do
         answer_count
     end
 
     def answer1 do
         input = Day5.input
-        question1(input, 0, false, false, 0)
+        question1(input, 0, false, false, @vowels, @bad, 0)
     end
 
 end
